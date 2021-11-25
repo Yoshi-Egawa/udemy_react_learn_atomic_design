61196a79ac7c439f38bf58f69a4f4caab9e635d6
Yoshi-Egawa <71691411+Yoshi-Egawa@users.noreply.github.com>
2021-11-26 00:09:27 +0900

    Initial commit

---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `よしぞー${val}`,
+    image: "https://source.unsplash.com/ypXur50LMqc",
+    email: "test@test.com",
+    tel: "000-000-0000",
+    company: {
+      name: "aaaa会社"
+    },
+    website: "gooogle.com"
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <SUserArea>
+        <h2>ユーザー一覧</h2>
+        <SearchInput />
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
